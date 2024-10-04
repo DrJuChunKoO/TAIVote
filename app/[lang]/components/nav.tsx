c9ec0fb833f92c943d6b586b762d45e9e47701b7
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-10-04 14:19:20 +0800

    updated nav style

--- app/[lang]/components/nav.tsx
+++ app/[lang]/components/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 export default function Nav({ lang }: { lang: Locale }) {
   return (
-    <nav className="p-3">
+    <nav className="mb-4 bg-gradient-to-b from-black/5 to-black/10 py-2">
       <div className="container flex flex-wrap items-center justify-between">
         <div className="text-2xl font-bold">TAIVote</div>
         <div className="flex gap-2">
@@ -13,14 +13,14 @@ export default function Nav({ lang }: { lang: Locale }) {
           >
             EN
           </Link>
-          {" | "}
+          <span className="select-none opacity-10">{" | "}</span>
           <Link
             href="/zh-TW"
             className={twMerge(lang === "zh-TW" && "font-bold text-white")}
           >
             中文
           </Link>
-          {" | "}
+          <span className="select-none opacity-10">{" | "}</span>
           <Link
             href="/ja-JP"
             className={twMerge(lang === "ja-JP" && "font-bold text-white")}
