6087c04f9d69509f139b31078f72b6de64039e09
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-10-07 13:46:08 +0800

    refactor question component

--- app/page.tsx
+++ app/page.tsx
@@ -3,143 +3,72 @@ import { twMerge } from "tailwind-merge";
 export default async function IndexPage() {
   return (
     <div className="container">
-      <div>
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌 - 1,990 票",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌 - 1,990 票",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌 - 1,990 票",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌 - 1,990 票",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-        <Question
-          question="咖哩要拌嗎"
-          options={[
-            {
-              text: "拌 - 1,990 票",
-              value: 1,
-              percentage: 10,
-            },
-            {
-              text: "不拌",
-              value: 0,
-              percentage: 90,
-            },
-          ]}
-        />
-      </div>
+      <Question>
+        <QuestionTitle>咖哩要拌嗎</QuestionTitle>
+        <Option text="拌" percentage={10} />
+        <Option text="不拌" percentage={90} />
+      </Question>
+      <Question>
+        <QuestionTitle>咖哩要拌嗎</QuestionTitle>
+        <Option text="拌" percentage={10} />
+        <Option text="不拌" percentage={90} />
+      </Question>
+      <Question>
+        <QuestionTitle>咖哩要拌嗎</QuestionTitle>
+        <Option text="拌" percentage={10} />
+        <Option text="不拌" percentage={90} />
+      </Question>
+      <Question>
+        <QuestionTitle>咖哩要拌嗎</QuestionTitle>
+        <Option text="拌" percentage={10} />
+        <Option text="不拌" percentage={90} />
+      </Question>
     </div>
   );
 }
-function Question({
-  question,
-  options,
+
+function Question({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="mb-4 rounded-xl border border-white/10 p-4 shadow-md transition-all hover:shadow-lg">
+      <div className="flex flex-col gap-4">{children}</div>
+    </div>
+  );
+}
+
+function QuestionTitle({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-xl font-bold text-gray-200">{children}</h2>;
+}
+function Option({
+  text,
+  percentage,
+  index = 0,
 }: {
-  question: string;
-  options: {
-    text: string;
-    value: number;
-    percentage: number;
-  }[];
+  text: string;
+  percentage: number;
+  index?: number;
 }) {
   const colors = [
-    "from-red-500 to-red-600",
-    "from-blue-500 to-blue-600",
-    "from-green-500 to-green-600",
-    "from-yellow-500 to-yellow-600",
-    "from-purple-500 to-pink-500",
+    "bg-gradient-to-b from-red-500 to-red-600",
+    "bg-gradient-to-b from-blue-500 to-blue-600",
+    "bg-gradient-to-b from-green-500 to-green-600",
+    "bg-gradient-to-b from-yellow-500 to-yellow-600",
+    "bg-gradient-to-b from-purple-500 to-pink-500",
   ];
 
   return (
-    <div className="mb-4 rounded-xl border border-white/10 p-4 shadow-md transition-all hover:shadow-lg">
-      <h2 className="mb-4 text-xl font-bold text-gray-200">{question}</h2>
-      <div className="flex flex-col gap-4">
-        {options.map((option, index) => (
-          <div key={index}>
-            <div className="mb-2 flex items-end justify-between">
-              <div className="font-medium text-gray-300"> {option.text} </div>
-              <div className="font-mono text-sm text-gray-400">
-                {option.percentage}%
-              </div>
-            </div>
-            <div className="bg-noise h-4 w-full rounded-full bg-gray-600 shadow-inner">
-              <div
-                className={twMerge(
-                  `h-4 rounded-full bg-gradient-to-b mix-blend-overlay transition-all duration-500 ease-in-out`,
-                  colors[index % colors.length],
-                )}
-                style={{ width: `${option.percentage}%` }}
-              />
-            </div>
-          </div>
-        ))}
+    <div>
+      <div className="mb-2 flex items-end justify-between">
+        <div className="font-medium text-gray-300">{text}</div>
+        <div className="font-mono text-sm text-gray-400">{percentage}%</div>
+      </div>
+      <div className="bg-noise h-4 w-full rounded-full bg-gray-600 shadow-inner">
+        <div
+          className={twMerge(
+            `h-4 rounded-full mix-blend-overlay transition-all duration-500 ease-in-out`,
+            colors[index % colors.length],
+          )}
+          style={{ width: `${percentage}%` }}
+        />
       </div>
     </div>
   );
