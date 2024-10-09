3087314e6aa1dc1b4280d5d075c75438b41b208c
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-10-09 14:17:58 +0800

    refactor layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 import Nav from "./components/nav";
-import VoteButton from "./components/vote-button";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-
+import SessionProvider from "./components/session-provider";
 export default async function RootLayout({
   children,
 }: {
@@ -17,21 +16,12 @@ export default async function RootLayout({
         <link rel="icon" href="/icon.png" type="image/png" />
       </head>
       <body className="flex h-[100svh] flex-col bg-[#282C33] text-[#C8CCD4]">
-        <NextIntlClientProvider messages={messages}>
-          <Nav />
-          <div className="relative flex-1 overflow-x-auto pt-8">
+        <SessionProvider>
+          <NextIntlClientProvider messages={messages}>
+            <Nav />
             {children}
-            <div className="gradient-blur translate-y-3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <VoteButton />
-        </NextIntlClientProvider>
+          </NextIntlClientProvider>
+        </SessionProvider>
       </body>
     </html>
   );
