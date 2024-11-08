a2f2edfe26c86c2b05d161567247ce12dbd3ac73
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-11-08 10:51:15 +0800

    added TFH link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,28 +1,45 @@
 "use client";
+import { Slot } from "@radix-ui/react-slot";
 export default function Footer() {
   return (
     <footer className="container">
       <div className="group text-center text-sm text-white text-opacity-50 transition-opacity">
         Support by{" "}
         <TipText tip="Emerging Technology Exchange Association">ETEA</TipText>{" "}
-        in partnership with <TipText tip="Tools for Humanity">TFH</TipText>
+        in partnership with{" "}
+        <TipText tip="Tools for Humanity">
+          <a
+            href="https://toolsforhumanity.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            TFH
+          </a>
+        </TipText>
       </div>
     </footer>
   );
 }
 function TipText({
+  asChild,
   children,
   tip,
+  ...attr
 }: {
+  asChild?: boolean;
   children: React.ReactNode;
   tip: string;
 }) {
+  const Comp = asChild ? Slot : "span";
   return (
-    <span className="group/item relative inline-block group-hover:underline">
+    <Comp
+      className="group/item relative inline-block group-hover:underline"
+      {...attr}
+    >
       <div className="glass-effect pointer-events-none absolute -top-7 right-[50%] h-max w-max origin-bottom translate-x-[50%] translate-y-3 scale-90 rounded-xl bg-white/10 px-3 py-1 opacity-0 transition-all group-hover/item:translate-y-0 group-hover/item:scale-100 group-hover/item:opacity-100">
         {tip}
       </div>
       {children}
-    </span>
+    </Comp>
   );
 }
